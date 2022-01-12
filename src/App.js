0f5ed3d66725417bf932e70caf114f0d0216c0a8
mayur25-cpuweb <bhatiamayur12@gmail.com>
2022-01-12 20:52:10 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Footer from './Components/footer';
 import Headernav from './Components/headernav';
-import Headernav1 from './Components/headernav1';
 import { BrowserRouter as Router, Link, Switch, Route, NavLink} from "react-router-dom";
 
 function App() {
